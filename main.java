0bf89ddba994a0e2e6ccb7f887dadc0114f0d9e3
Abhiram-JV <jalduvabhiram@gmail.com>
2026-01-06 09:51:34 +0530

    Feature update: Added version tracking and enhanced messages

--- main.java
+++ main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git Version Control!");
         System.out.println("This is the initial version of the project.");
+        
+        // Feature Update: Added new functionality
+        System.out.println("Feature Update: Enhanced with version tracking");
+        System.out.println("Version: 2.0");
     }
 }
